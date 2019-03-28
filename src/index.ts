3612e77bbc89d0b9b0e7776d88f5184aeb96220f
Alan Olivares <richard.alan.oli@gmail.com>
2019-03-28 11:04:44 -0600

    I removed useless comments

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,3 @@ export {
 	extractGeneralData
 };
 
-
-
-// import CfdiExtractData from './CfdiExtractData';
-
-
-// console.log(
-// CfdiExtractData.extractGeneralData({
-// 	path: '/Users/ricaralan/Documents/proyects/onefacture-desktop/XMLs/OIRR940203TH7/emitidos/2019/03 MAR/C48F6D54-9A4D-4AC6-BDC7-3DBFB9851275.xml'
-// })
-// );
-
-
